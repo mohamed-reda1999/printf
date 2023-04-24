09fb9e75b953a4d1c8a3794b5f04b26af4160750
NadaFarouk01 <nadafarouk18@gmail.com>
2023-04-23 19:10:02 -0700

    tasks

--- git_width.c
+++ git_width.c
@@ -2,8 +2,11 @@
 
 /**
  * get_width - Calculates the width for printing
+ *
  * @format: Formatted string in which to print the arguments.
+ *
  * @i: List of arguments to be printed.
+ *
  * @list: list of arguments.
  *
  * Return: width.
